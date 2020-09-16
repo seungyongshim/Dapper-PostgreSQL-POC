4b7b3b7b3f1d31543f4ca10d419ecfc2c32e5e3b
심승용 <seungyong.shim@mirero.co.kr>
2020-09-16 09:52:32 +0900

    1

---
+++ 1. dapper/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Dapper;
+using Npgsql;
+using System.Text.Json;
+using System.Collections.Generic;
+
+namespace _1._dapper
+{
+    public class UserInformation
+    {
+        public long USER_ID { get; set; }
+        public string PASSWORD { get; set; }
+        public string USER_NAME { get; set; }
+        public string USER_GROUP { get; set; }
+        public int DEPARTMENT { get; set; }
+        public int AUTHORITY { get; set; }
+
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var connection = new NpgsqlConnection("Host=localhost;Database=mlsdb;Username=mlsuser;Password=mlsuser;");
+
+
+
+            connection.Execute(@"INSERT INTO USERS (PASSWORD, USER_NAME, USER_GROUP, DEPARTMENT, AUTHORITY)" + 
+                                "VALUES (:PASSWORD, :USER_NAME, :USER_GROUP, :DEPARTMENT, :AUTHORITY)",
+                new UserInformation
+                {
+                    PASSWORD = "9874",
+                    USER_NAME = "MARY",
+                    USER_GROUP = "MIRERO",
+                    DEPARTMENT = 1,
+                    AUTHORITY = 2
+                });
+
+            var persons = connection.Query<UserInformation>("SELECT * FROM USERS");
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
+        }
+
+        public void AddWidgets(IEnumerable<UserInformation> Users)
+        {
+            
+        }
+    }
+}
---
+++ 1. dapper/UserInfomationGateway.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace _1._dapper
+{
+    public class UserInfomationGateway
+    {
+        public UserInfomationGateway(Func<DbConnection> dbConnect)
+        {
+            DbConnect = dbConnect;
+        }
+
+        Func<DbConnection> DbConnect { get; }
+
+        public IEnumerable<UserInformation> FindAll()
+        {
+            using (var connection = DbConnect())
+            {
+                return connection.Query<UserInformation>("SELECT * FROM USERS");
+            }
+        }
+    }
+}
